05b82c29448360cbd0befa53202112e9bfa6364e
xuliujian@gmail.com <xuliujian@gmail.com>
2018-08-14 00:16:57 +0800

    修改导航图标显示

--- Sanctum/Sanctum.Web/Modules/Administration/AdministrationNavigation.cs
+++ Sanctum/Sanctum.Web/Modules/Administration/AdministrationNavigation.cs
@@ -3,7 +3,7 @@ using Administration = Sanctum.Administration.Pages;
 
 [assembly: NavigationMenu(int.MaxValue, "Administration", icon: "fa-desktop")]
 [assembly: NavigationLink(int.MaxValue, "Administration/Languages", typeof(Administration.LanguageController), icon: "fa-comments")]
-[assembly: NavigationLink(int.MaxValue, "Administration/Translations", typeof(Administration.TranslationController), icon: "fa-comment-o")]
+[assembly: NavigationLink(int.MaxValue, "Administration/Translations", typeof(Administration.TranslationController), icon: "fa-globe")]
 [assembly: NavigationLink(int.MaxValue, "Administration/Sergen", typeof(Administration.SergenController), icon: "fa-magic")]
 [assembly: NavigationLink(int.MaxValue, "Administration/Roles", typeof(Administration.RoleController), icon: "fa-lock")]
 [assembly: NavigationLink(int.MaxValue, "Administration/User Management", typeof(Administration.UserController), icon: "fa-users")]
